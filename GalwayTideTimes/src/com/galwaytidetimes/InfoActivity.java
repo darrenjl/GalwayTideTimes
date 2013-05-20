7b086fb39fb5877c2f97d04deedfe5442aceec31
Darren Lyons <d.lyons88@gmail.com>
2013-05-20 11:21:30 +0100

    Adding code to prevent recreation of activity on orientation change to info activity. For practice, not merging to master branch

--- GalwayTideTimes/src/com/galwaytidetimes/InfoActivity.java
+++ GalwayTideTimes/src/com/galwaytidetimes/InfoActivity.java
@@ -1,9 +1,11 @@
 package com.galwaytidetimes;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.widget.TextView;
 import com.google.analytics.tracking.android.EasyTracker;
 
@@ -11,9 +13,12 @@ import com.google.analytics.tracking.android.EasyTracker;
  * Created by Darren on 16/05/13.
  */
 public class InfoActivity extends Activity {
+	private static String TAG = "GTT";
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_info);
         TextView descriptionTextView = (TextView) findViewById(R.id.info_text_view);
         descriptionTextView.setText(Html.fromHtml(getString(R.string.info_string)));
@@ -31,5 +36,11 @@ public class InfoActivity extends Activity {
     public void onStop() {
         super.onStop();
         EasyTracker.getInstance().activityStop(this); // Add this method.
+    }
+    
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        setContentView(R.layout.activity_info);
     }
 }
